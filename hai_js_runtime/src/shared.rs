cb12d8201bc7453beeb110e92f43736c0a0c1dab
Icemic <bingfeng.web@gmail.com>
2022-02-20 11:02:36 +0800

    feat(runtime): support custom ptr in isolate slot

--- hai_js_runtime/src/shared.rs
+++ hai_js_runtime/src/shared.rs
@@ -1,5 +1,10 @@
 use futures::task::AtomicWaker;
-use std::{cell::RefCell, rc::Rc};
+use std::{
+    cell::RefCell,
+    ffi::c_void,
+    rc::Rc,
+    sync::{Arc, Mutex},
+};
 
 use super::{module::ModuleLoader, timer::TimerScheduler};
 
@@ -7,17 +12,23 @@ pub struct Shared {
     pub module_loader: Rc<RefCell<ModuleLoader>>,
     pub timer: Rc<RefCell<TimerScheduler>>,
     pub waker: AtomicWaker,
+    /// external state binding to js runtime
+    /// which makes injected methods read or write custom data
+    pub state: *const c_void,
 }
 
 impl Shared {
-    pub fn new() -> Self {
+    pub fn new<T>(state: Arc<Mutex<T>>) -> Self {
         let module_loader = ModuleLoader::new();
         let timer = TimerScheduler::new();
+
+        let state = Arc::into_raw(state) as *const c_void;
 
         Shared {
             module_loader: Rc::new(RefCell::new(module_loader)),
             timer: Rc::new(RefCell::new(timer)),
             waker: AtomicWaker::new(),
+            state,
         }
     }
 
@@ -27,5 +38,12 @@ impl Shared {
 
     pub fn timer(&self) -> Rc<RefCell<TimerScheduler>> {
         self.timer.clone()
+    }
+
+    pub fn state<T>(&self) -> Arc<Mutex<T>> {
+        // let boxed = unsafe { transmute::<*mut c_void, Box<T>>(self.state) };
+        // boxed.as_ref().clone()
+        let ptr = self.state as *const Mutex<T>;
+        unsafe { Arc::from_raw(ptr) }
     }
 }
