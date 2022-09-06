1e99c399f2cf7c28f29846792901aad858df7a55
Icemic <bingfeng.jw@gmail.com>
2022-09-06 12:57:34 +0800

    feat(core): get entry_dir from pal

--- hai_pal/src/env.rs
+++ hai_pal/src/env.rs
@@ -1,3 +1,5 @@
+use std::{env, path::PathBuf};
+
 #[cfg(not(target_arch = "wasm32"))]
 pub fn setup() {
     use dotenv::dotenv;
@@ -8,4 +10,10 @@ pub fn setup() {
 #[cfg(target_arch = "wasm32")]
 pub fn setup() {
     // unimplemented!("env module not implemented for wasm target");
+}
+
+pub fn entry_dir() -> PathBuf {
+    let entry_dir =
+        env::var("HAI_ENTRY").unwrap_or(env::current_dir().unwrap().to_str().unwrap().to_string());
+    PathBuf::from(entry_dir)
 }
