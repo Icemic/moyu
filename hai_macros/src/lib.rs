51f43c73c4b4a500c2538c53020115afbc3be0d4
Icemic <bingfeng.web@gmail.com>
2023-03-01 19:50:06 +0800

    feat(core): add js value convert utils based on serde_v8

--- hai_macros/src/lib.rs
+++ hai_macros/src/lib.rs
@@ -1,3 +1,4 @@
+mod hai_bindgen;
 mod node;
 
 use proc_macro::TokenStream;
@@ -5,4 +6,9 @@ use proc_macro::TokenStream;
 #[proc_macro_attribute]
 pub fn node(args: TokenStream, struct_body: TokenStream) -> TokenStream {
     node::node(args, struct_body)
+}
+
+#[proc_macro_attribute]
+pub fn hai_bindgen(args: TokenStream, func_body: TokenStream) -> TokenStream {
+    hai_bindgen::entry(args, func_body)
 }
