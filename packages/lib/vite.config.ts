bb31ec036904a29bc97f5e5ed9b6e18043891bff
Icemic <bingfeng.web@gmail.com>
2023-04-12 18:50:48 +0800

    fix(lib): build example

--- packages/lib/vite.config.ts
+++ packages/lib/vite.config.ts
@@ -1,3 +1,4 @@
+import { resolve } from 'path';
 import { defineConfig } from 'vite';
 import react from '@vitejs/plugin-react';
 
@@ -11,25 +12,44 @@ export default defineConfig({
     }),
   ],
 
+  define: {
+    'process.env': {
+      NODE_ENV: 'production',
+    },
+  },
+
   envPrefix: 'EXPORT_',
 
   build: {
-    manifest: true,
+    manifest: false,
     minify: 'esbuild',
     sourcemap: true,
+    target: 'es2022',
+    assetsInlineLimit: 0,
+    lib: {
+      entry: resolve(__dirname, 'src/lib.ts'),
+      fileName: 'hai',
+      formats: ['es'],
+    },
+    rollupOptions: {
+      input: {
+        reactExample: resolve(__dirname, 'examples/react/index.tsx'),
+        bunnyMark: resolve(__dirname, 'examples/bunnyMark/index.ts'),
+      },
+      output: {
+        entryFileNames: '[name]/index.js',
+        inlineDynamicImports: false,
+        format: 'es',
+        manualChunks: {},
+        preserveModules: false,
+      },
+    },
   },
 
   optimizeDeps: {
     exclude: [],
   },
 
-  css: {
-    preprocessorOptions: {
-      less: {
-        javascriptEnabled: true,
-      },
-    },
-  },
   server: {
     host: '0.0.0.0',
     port: 3020,
