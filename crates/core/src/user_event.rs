8b0dcfff89d696e35b38fadd7e7f976e0990c20c
Icemic <bingfeng.web@gmail.com>
2023-05-26 19:11:55 +0800

    feat(core): revert latest changes

--- crates/core/src/user_event.rs
+++ crates/core/src/user_event.rs
@@ -1,12 +1,14 @@
+use std::ffi::c_void;
+
 #[allow(dead_code)]
 #[derive(Debug, Clone)]
-pub enum UserEvent<T = ()> {
+pub enum UserEvent {
     // logical_width, logical_height, factor
     ResizeWindow(f64, f64, Option<f64>),
     WindowState(WindowState),
     SetTitle(String),
     Quit,
-    Custom(T),
+    Custom(*mut c_void),
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
