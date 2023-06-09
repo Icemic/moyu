c278557def7c022e2a1e90b0b7c2fb646b89e612
Icemic <bingfeng.web@gmail.com>
2023-06-09 19:44:07 +0800

    fix(core): wrong intial `_current_update_id` value

--- crates/core/src/nodes/container.rs
+++ crates/core/src/nodes/container.rs
@@ -28,7 +28,7 @@ impl Container {
             skew: Point::default(),
 
             _update_id: 0,
-            _current_update_id: 1,
+            _current_update_id: 0,
 
             transform: Transform::default(),
             global_transform: Transform::default(),
