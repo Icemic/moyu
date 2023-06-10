8c145f4a7e2b3844be21b6ebb8c75e53b499c8b0
Icemic <bingfeng.web@gmail.com>
2023-06-11 02:23:56 +0800

    feat(macros): remove proc-macro `node`

--- crates/macros/src/lib.rs
+++ crates/macros/src/lib.rs
@@ -1,12 +1,6 @@
 mod hai_bindgen;
-mod node;
 
 use proc_macro::TokenStream;
-
-#[proc_macro_attribute]
-pub fn node(args: TokenStream, struct_body: TokenStream) -> TokenStream {
-    node::node(args, struct_body)
-}
 
 #[proc_macro_attribute]
 pub fn hai_bindgen(args: TokenStream, func_body: TokenStream) -> TokenStream {
