8f91f6cd3dad74b2279e0d963ac55ce08eb8995a
Icemic <bingfeng.web@gmail.com>
2023-06-11 23:19:09 +0800

    fix(core): move `NodeProps` to be with `Node`

--- crates/core/src/traits/node.rs
+++ crates/core/src/traits/node.rs
@@ -1,30 +1,11 @@
-use serde::{Deserialize, Serialize};
 use std::{any::Any, fmt::Debug};
 
 use crate::nodes::NodeBase;
-#[cfg(all(not(feature = "web"), feature = "js_runtime"))]
 use crate::utils::convert::JSValue;
-
-#[derive(Serialize, Deserialize)]
-#[serde(rename_all = "camelCase")]
-pub struct NodeProps {
-    pub anchor: Option<[f64; 2]>,
-    pub pivot: Option<[f64; 2]>,
-    pub x: Option<f64>,
-    pub y: Option<f64>,
-    pub scale: Option<f64>,
-    pub scale_x: Option<f64>,
-    pub scale_y: Option<f64>,
-    pub rotation: Option<f64>,
-    pub skew: Option<f64>,
-    pub skew_x: Option<f64>,
-    pub skew_y: Option<f64>,
-}
 
 pub trait Node: NodeBaseTrait + Send + Sync + Debug {
     fn node_type(&self) -> &'static str;
 
-    #[cfg(all(not(feature = "web"), feature = "js_runtime"))]
     fn update_properties(&mut self, _props: &mut JSValue) {
         // defaults to do nothing
     }
