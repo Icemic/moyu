b0349c0c432493d4555ce39d7d9d8df1cedb0a2d
Icemic <bingfeng.web@gmail.com>
2023-06-11 23:19:09 +0800

    fix(core): move `NodeProps` to be with `Node`

--- crates/core/src/nodes/node.rs
+++ crates/core/src/nodes/node.rs
@@ -196,8 +196,6 @@ impl NodeBase {
     #[cfg(all(not(feature = "web"), feature = "js_runtime"))]
     #[inline]
     pub fn update_properties(&mut self, props: &mut JSValue) {
-        use crate::traits::NodeProps;
-
         let props: NodeProps = from_js(props).unwrap();
 
         if let Some(x) = props.x {
@@ -352,4 +350,21 @@ impl NodeBase {
             self._current_update_id = self._update_id;
         }
     }
+}
+
+use serde::{Deserialize, Serialize};
+#[derive(Serialize, Deserialize)]
+#[serde(rename_all = "camelCase")]
+pub struct NodeProps {
+    pub anchor: Option<[f64; 2]>,
+    pub pivot: Option<[f64; 2]>,
+    pub x: Option<f64>,
+    pub y: Option<f64>,
+    pub scale: Option<f64>,
+    pub scale_x: Option<f64>,
+    pub scale_y: Option<f64>,
+    pub rotation: Option<f64>,
+    pub skew: Option<f64>,
+    pub skew_x: Option<f64>,
+    pub skew_y: Option<f64>,
 }
