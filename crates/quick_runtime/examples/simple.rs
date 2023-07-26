edad4b6d92115ba949c5241f9544da86cfa4b23f
Icemic <bingfeng.web@gmail.com>
2023-07-27 03:01:10 +0800

    feat(quick_runtime): impl setTimeout/setInterval/clearTimeout/clearInterval

--- crates/quick_runtime/examples/simple.rs
+++ crates/quick_runtime/examples/simple.rs
@@ -9,17 +9,27 @@ async fn main() {
 
     hai_pal::platform::setup();
 
-    std::thread::spawn(move || {
-        let vm = setup_vm();
+    let local = tokio::task::LocalSet::new();
 
-        vm.context()
-            .eval("console.log('Hello %s!', 'World')")
-            .unwrap();
+    local
+        .run_until(async {
+            let vm = setup_vm();
 
-        if let Err(err) = vm.prepare_entry() {
-            println!("{:?}", err);
-        };
-    })
-    .join()
-    .unwrap();
+            vm.context()
+                .eval("console.log('Hello %s!', 'World')")
+                .unwrap();
+
+            vm.context()
+                .eval("var x = setInterval(() => console.log('Hello %s!', 'World'), 1000); setTimeout(() => clearTimeout(x), 1500)")
+                .unwrap();
+
+            // if let Err(err) = vm.prepare_entry() {
+            //     println!("{:?}", err);
+            // };
+
+            // block
+            let future = std::future::pending();
+            let () = future.await;
+        })
+        .await;
 }
