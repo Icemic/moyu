eb8c59c34265a269c775f27686c31c84f7c55272
Icemic <bingfeng.web@gmail.com>
2023-07-27 03:01:10 +0800

    feat(quick_runtime): impl setTimeout/setInterval/clearTimeout/clearInterval

--- crates/quick_runtime/src/vm.rs
+++ crates/quick_runtime/src/vm.rs
@@ -1,13 +1,21 @@
+use std::collections::HashMap;
 use std::ptr::null_mut;
+use std::sync::Arc;
+use std::time::Duration;
 
 use hai_pal::env::entry_dir;
-use quickjspp::{Context, ExecutionError};
+use hai_pal::task::{spawn_local, JoinHandle};
+use quickjspp::{Context, ExecutionError, JsFunction};
+use std::sync::Mutex;
 
 use crate::console::log_handler;
 use crate::module::{module_loader, module_normalize};
 
+static mut TIMER_ID: i32 = 0;
+
 pub struct QuickVM {
     context: Context,
+    timer_handles: Arc<Mutex<HashMap<i32, JoinHandle<()>>>>,
 }
 
 unsafe impl Send for QuickVM {}
@@ -26,7 +34,77 @@ impl QuickVM {
             null_mut(),
         );
 
-        Self { context }
+        let timer_handles = Arc::new(Mutex::new(HashMap::new()));
+
+        {
+            let timer_handles = timer_handles.clone();
+            context
+                .add_callback("setTimeout", move |callback: JsFunction, duration: i32| {
+                    let timer_id = unsafe {
+                        TIMER_ID += 1;
+                        TIMER_ID
+                    };
+
+                    let handle = {
+                        let timer_handles = timer_handles.clone();
+                        spawn_local(async move {
+                            tokio::time::sleep(Duration::from_millis(duration as u64)).await;
+                            callback.call(vec![]).unwrap();
+                            timer_handles.lock().unwrap().remove(&timer_id);
+                        })
+                    };
+
+                    timer_handles.lock().unwrap().insert(timer_id, handle);
+
+                    return timer_id;
+                })
+                .unwrap();
+        }
+
+        {
+            let timer_handles = timer_handles.clone();
+            context
+                .add_callback("setInterval", move |callback: JsFunction, duration: i32| {
+                    let timer_id = unsafe {
+                        TIMER_ID += 1;
+                        TIMER_ID
+                    };
+
+                    let handle = spawn_local(async move {
+                        loop {
+                            tokio::time::sleep(Duration::from_millis(duration as u64)).await;
+                            callback.call(vec![]).unwrap();
+                        }
+                    });
+
+                    timer_handles.lock().unwrap().insert(timer_id, handle);
+
+                    return timer_id;
+                })
+                .unwrap();
+        }
+
+        {
+            let timer_handles = timer_handles.clone();
+            let clear_timer = move |timer_id: i32| {
+                if let Some(handle) = timer_handles.lock().unwrap().remove(&timer_id) {
+                    handle.abort();
+                }
+
+                return 0;
+            };
+
+            // clearInterval is in fact the same as clearTimeout
+            context
+                .add_callback("clearTimeout", clear_timer.clone())
+                .unwrap();
+            context.add_callback("clearInterval", clear_timer).unwrap();
+        }
+
+        Self {
+            context,
+            timer_handles,
+        }
     }
 
     pub fn context(&self) -> &Context {
@@ -46,5 +124,14 @@ impl QuickVM {
         };
 
         self.context.run_module(&module_name)
+    }
+}
+
+impl Drop for QuickVM {
+    fn drop(&mut self) {
+        // abort all pending timers
+        for (_, handle) in self.timer_handles.lock().unwrap().drain() {
+            handle.abort();
+        }
     }
 }
