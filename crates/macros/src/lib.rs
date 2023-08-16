84871ca1d3cdaaca8d192e9b3a36c22f1a680312
Icemic <bingfeng.web@gmail.com>
2023-08-16 12:34:00 +0800

    fix(macros): compiling condition

--- crates/macros/src/lib.rs
+++ crates/macros/src/lib.rs
@@ -2,6 +2,7 @@ mod bindgen;
 
 use proc_macro::TokenStream;
 
+#[cfg(any(feature = "quickjs", feature = "v8"))]
 #[proc_macro_attribute]
 pub fn hai_bindgen(args: TokenStream, func_body: TokenStream) -> TokenStream {
     bindgen::entry(args, func_body)
