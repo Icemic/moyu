8115e91b306c16887b408dee47712baf94d42fb5
Icemic <bingfeng.web@gmail.com>
2023-08-19 19:39:37 +0800

    fix(lib): react type

--- packages/lib/src/declaration.ts
+++ packages/lib/src/declaration.ts
@@ -1,4 +1,5 @@
 import { ReactNode } from 'react';
+import * as PropTypes from 'prop-types';
 
 export type DetailedHaiProps<E extends HaiNodeAttributes> = E;
 
@@ -30,19 +31,134 @@ export interface HaiYUVSpriteAttribute extends HaiNodeAttributes {
   area?: [number, number, number, number];
 }
 
-export interface HaiPlayerAttribute extends HaiSpriteAttribute {
+export interface HaiVideoAttribute extends HaiSpriteAttribute {
   src: string;
   area?: [number, number, number, number];
   autoplay?: boolean;
 }
 
-declare global {
+export interface LayoutSytle {
+  /// the writing direction of the text in the box,
+  /// only `Horizontal` (right-to-left) or `Vertical` (top-to-bottom) is valid.
+  direction?: 'horizontal' | 'vertical';
+  /// the width of box.
+  boxWidth?: number;
+  /// the height of box.
+  boxHeight?: number;
+  /// the size of the glyph grid which each character be fit to, usually equals to `font_size`.
+  glyphGridSize?: number;
+}
+
+export interface StrokeStyle {
+  strokeColor?: number | string;
+  strokeWidth?: number;
+}
+
+export interface ShadowStyle {
+  shadowColor?: number | string;
+  shadowOffsetX?: number;
+  shadowOffsetY?: number;
+  shadowBlur?: number;
+  shadowWidth?: number;
+}
+
+export interface TextStyle {
+  // pub font_face: Font
+  fontSize?: number;
+  fillColor?: number | string;
+  lineHeight?: number;
+  indent?: number;
+  stroke?: StrokeStyle;
+  shadow?: ShadowStyle;
+}
+
+export interface HaiTextAttribute extends HaiNodeAttributes {
+  text?: string;
+  layoutStyle?: LayoutSytle;
+  textStyle?: TextStyle;
+}
+
+declare module 'react' {
+  /* eslint-disable */
   namespace JSX {
+    interface Element extends React.ReactElement<any, any> {}
+    interface ElementClass extends React.Component<any> {
+      render(): React.ReactNode;
+    }
+    interface ElementAttributesProperty {
+      props: {};
+    }
+    interface ElementChildrenAttribute {
+      children: {};
+    }
+
+    // naked 'any' type in a conditional type will short circuit and union both the then/else branches
+    // so boolean is only resolved for T = any
+    type IsExactlyAny<T> = boolean extends (T extends never ? true : false) ? true : false;
+
+    type ExactlyAnyPropertyKeys<T> = { [K in keyof T]: IsExactlyAny<T[K]> extends true ? K : never }[keyof T];
+    type NotExactlyAnyPropertyKeys<T> = Exclude<keyof T, ExactlyAnyPropertyKeys<T>>;
+
+    // Try to resolve ill-defined props like for JS users: props can be any, or sometimes objects with properties of type any
+    type MergePropTypes<P, T> =
+      // Distribute over P in case it is a union type
+      P extends any
+        ? // If props is type any, use propTypes definitions
+          IsExactlyAny<P> extends true
+          ? T
+          : // If declared props have indexed properties, ignore inferred props entirely as keyof gets widened
+          string extends keyof P
+          ? P
+          : // Prefer declared types which are not exactly any
+            Pick<P, NotExactlyAnyPropertyKeys<P>> &
+              // For props which are exactly any, use the type inferred from propTypes if present
+              Pick<T, Exclude<keyof T, NotExactlyAnyPropertyKeys<P>>> &
+              // Keep leftover props not specified in propTypes
+              Pick<P, Exclude<keyof P, keyof T>>
+        : never;
+
+    type InexactPartial<T> = { [K in keyof T]?: T[K] | undefined };
+
+    // Any prop that has a default prop becomes optional, but its type is unchanged
+    // Undeclared default props are augmented into the resulting allowable attributes
+    // If declared props have indexed properties, ignore default props entirely as keyof gets widened
+    // Wrap in an outer-level conditional type to allow distribution over props that are unions
+    type Defaultize<P, D> = P extends any
+      ? string extends keyof P
+        ? P
+        : Pick<P, Exclude<keyof P, keyof D>> &
+            InexactPartial<Pick<P, Extract<keyof P, keyof D>>> &
+            InexactPartial<Pick<D, Exclude<keyof D, keyof P>>>
+      : never;
+
+    type ReactManagedAttributes<C, P> = C extends { propTypes: infer T; defaultProps: infer D }
+      ? Defaultize<MergePropTypes<P, PropTypes.InferProps<T>>, D>
+      : C extends { propTypes: infer T }
+      ? MergePropTypes<P, PropTypes.InferProps<T>>
+      : C extends { defaultProps: infer D }
+      ? Defaultize<P, D>
+      : P;
+
+    // We can't recurse forever because `type` can't be self-referential;
+    // let's assume it's reasonable to do a single React.lazy() around a single React.memo() / vice-versa
+    type LibraryManagedAttributes<C, P> = C extends
+      | React.MemoExoticComponent<infer T>
+      | React.LazyExoticComponent<infer T>
+      ? T extends React.MemoExoticComponent<infer U> | React.LazyExoticComponent<infer U>
+        ? ReactManagedAttributes<U, P>
+        : ReactManagedAttributes<T, P>
+      : ReactManagedAttributes<C, P>;
+
+    interface IntrinsicAttributes extends React.Attributes {}
+    interface IntrinsicClassAttributes<T> extends React.ClassAttributes<T> {}
+    /* eslint-enable */
+
     interface IntrinsicElements {
       container: DetailedHaiProps<HaiContainerAttributes>;
       sprite: DetailedHaiProps<HaiSpriteAttribute>;
       yuvsprite: DetailedHaiProps<HaiYUVSpriteAttribute>;
-      player: DetailedHaiProps<HaiPlayerAttribute>;
+      video: DetailedHaiProps<HaiVideoAttribute>;
+      text: DetailedHaiProps<HaiTextAttribute>;
     }
   }
 }
