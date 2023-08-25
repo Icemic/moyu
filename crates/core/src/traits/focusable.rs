34a8286c4c6b5b76bb47b536287408e0195dc4f7
Icemic <bingfeng.web@gmail.com>
2023-08-25 21:20:25 +0800

    fix(core): hit test handles anchor incorrectly

--- crates/core/src/traits/focusable.rs
+++ crates/core/src/traits/focusable.rs
@@ -1,7 +1,11 @@
 use super::{Node, RendererUpdatePayload};
 
 pub trait Focusable: Node {
-    /// check whether the input point is on the node-like,
-    /// coordinate of the point is relative to its parent
+    /**
+       Check whether the input point is on the node-like, coordinate of the point is relative to its parent.
+
+       You may need to select property width and height, then handle `anchor` property to make it work.
+       Or, if it is not square, you may need to do something more complex to calculate the correct area.
+    */
     fn contains(&self, x: f32, y: f32, payload: &RendererUpdatePayload) -> bool;
 }
