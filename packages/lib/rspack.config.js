62503b3de39bbbe938c14f95ad3caaa2a911d568
Icemic <bingfeng.web@gmail.com>
2024-02-11 12:39:20 +0800

    Try to use rspack

---
+++ packages/lib/rspack.config.js
@@ -0,0 +1,80 @@
+const rspack = require('@rspack/core');
+// const refreshPlugin = require("@rspack/plugin-react-refresh");
+const isDev = process.env.NODE_ENV === 'development';
+/**
+ * @type {import('@rspack/cli').Configuration}
+ */
+module.exports = {
+  context: __dirname,
+  entry: {
+    bunnymark: './examples/bunnyMark/index.ts',
+    react: './examples/react/index.tsx',
+    demo: {
+      import: './examples/demo/index.tsx',
+      baseUri: '/demo11',
+      publicPath: '/demo11/',
+    },
+  },
+  resolve: {
+    extensions: ['...', '.ts', '.tsx', '.jsx'],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.svg$/,
+        type: 'asset',
+      },
+      {
+        test: /\.(jsx?|tsx?)$/,
+        use: [
+          {
+            loader: 'builtin:swc-loader',
+            options: {
+              sourceMap: true,
+              jsc: {
+                parser: {
+                  syntax: 'typescript',
+                  tsx: true,
+                },
+                transform: {
+                  react: {
+                    runtime: 'automatic',
+                    development: isDev,
+                    refresh: false,
+                  },
+                },
+              },
+              env: {
+                targets: ['chrome >= 87', 'edge >= 88', 'firefox >= 78', 'safari >= 14'],
+              },
+            },
+          },
+        ],
+      },
+    ],
+  },
+  devServer: {
+    hot: false,
+    liveReload: false,
+    webSocketServer: false,
+    static: {
+      publicPath: '/',
+      directory: './examples/demo',
+    },
+  },
+  builtins: {
+    react: {
+      refresh: false,
+    },
+  },
+  plugins: [
+    new rspack.DefinePlugin({
+      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV),
+    }),
+    // new rspack.ProgressPlugin({}),
+    // new rspack.HtmlRspackPlugin({
+    //   template: './index.html',
+    // }),
+    // isDev ? new refreshPlugin() : null
+  ].filter(Boolean),
+};
