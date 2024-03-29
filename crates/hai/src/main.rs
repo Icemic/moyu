a4d86b6177811ac83692614b1f509f45db94f39b
Icemic <bingfeng.web@gmail.com>
2024-03-30 01:11:14 +0800

    feat: makes yuv sprite optional, and default to disabled

--- crates/hai/src/main.rs
+++ crates/hai/src/main.rs
@@ -6,7 +6,7 @@ use hai_core::winit::dpi::PhysicalPosition;
 use hai_core::winit::event::Event;
 use hai_core::winit::window::Window;
 use hai_core::{create_hai_core, setup};
-use hai_nodes::renderer::{SpriteRenderer, TextRenderer, YUVSpriteRenderer};
+use hai_nodes::renderer::{SpriteRenderer, TextRenderer};
 use hai_ops::spawn::spawn_runtime_with_core;
 use hai_pal::{env, logger, platform};
 
@@ -56,7 +56,6 @@ fn main_entry() {
                     let (instance, surface, device, queue, config) = create_wgpu_surface(&_window);
 
                     let sprite_renderer = SpriteRenderer::new(&device, &config);
-                    let yuv_sprite_renderer = YUVSpriteRenderer::new(&device, &config);
                     let text_renderer = TextRenderer::new(&device, &config);
                     // use sprite renderer on video node
                     // #[cfg(feature = "video")]
@@ -74,8 +73,6 @@ fn main_entry() {
 
                     // core.register_renderer("null".to_string(), null_renderer);
                     _core.register_renderer("sprite".to_string(), Box::new(sprite_renderer));
-                    _core
-                        .register_renderer("yuv_sprite".to_string(), Box::new(yuv_sprite_renderer));
                     _core.register_renderer("text".to_string(), Box::new(text_renderer));
 
                     // #[cfg(feature = "video")]
--- crates/nodes/src/renderer/mod.rs
+++ crates/nodes/src/renderer/mod.rs
@@ -1,9 +1,11 @@
 mod sprite;
 #[cfg(feature = "text")]
 mod text;
+#[cfg(feature = "yuv_sprite")]
 mod yuv_sprite;
 
 pub use sprite::*;
 #[cfg(feature = "text")]
 pub use text::*;
+#[cfg(feature = "yuv_sprite")]
 pub use yuv_sprite::*;
