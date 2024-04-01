4150315c1c6fef756533a58dd699d7e5dd317f76
Icemic <bingfeng.web@gmail.com>
2024-04-02 00:32:50 +0800

    feat(core): adds void renderer

--- crates/core/src/nodes/mod.rs
+++ crates/core/src/nodes/mod.rs
@@ -1,7 +1,9 @@
 mod container;
 mod node;
+mod null_renderer;
 mod texture;
 
 pub use container::*;
 pub use node::*;
+pub use null_renderer::*;
 pub use texture::*;
---
+++ crates/core/src/nodes/null_renderer.rs
@@ -0,0 +1,54 @@
+use std::sync::Arc;
+use wgpu::util::StagingBelt;
+use wgpu::*;
+
+use crate::traits::{Node, Renderer, RendererUpdatePayload};
+
+/// A renderer that does nothing.
+///
+/// This is used when your node don't need to render anything.
+pub struct VoidRenderer {}
+
+impl VoidRenderer {
+    pub fn new(_: &Arc<Device>, _: &SurfaceConfiguration) -> Self {
+        Self {}
+    }
+}
+
+impl Renderer for VoidRenderer {
+    fn name(&self) -> &'static str {
+        "void"
+    }
+
+    fn render_pipeline(&self) -> &RenderPipeline {
+        unreachable!();
+    }
+
+    fn bind_group_layout(&self) -> &BindGroupLayout {
+        unreachable!();
+    }
+
+    fn update(
+        &mut self,
+        _: &mut dyn Node,
+        _: &Arc<Device>,
+        _: &Arc<Queue>,
+        _: &mut CommandEncoder,
+        _: &mut StagingBelt,
+        _: &RendererUpdatePayload,
+    ) {
+        // do nothing
+    }
+
+    fn begin(&self) {}
+    fn finish(&self) {}
+
+    fn render<'a, 'b: 'a>(
+        &'b self,
+        _: &Arc<Device>,
+        _: &Arc<Queue>,
+        _: &mut RenderPass<'a>,
+        _: &'b dyn Node,
+    ) {
+    }
+}
