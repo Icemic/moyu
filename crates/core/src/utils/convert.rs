ca955bec479e5687d062217830a57568fcc2cf66
Icemic <bingfeng.web@gmail.com>
2024-07-03 01:00:19 +0800

    feat(core): Add create_promise function for creating promises from Futures

--- crates/core/src/utils/convert.rs
+++ crates/core/src/utils/convert.rs
@@ -1,6 +1,7 @@
 #[cfg(feature = "web")]
 pub type JSValue = wasm_bindgen::JsValue;
 
+use hai_runtime::quickjs_rusty::OwnedJsPromise;
 #[cfg(all(not(feature = "web"), feature = "quickjs"))]
 use hai_runtime::quickjs_rusty::{JSContext, OwnedJsValue};
 
@@ -43,4 +44,37 @@ pub fn to_js<T: serde::Serialize>(
 #[cfg(feature = "web")]
 pub fn to_js<'a, T: serde::Serialize>(value: &T) -> Result<JSValue, serde_wasm_bindgen::Error> {
     serde_wasm_bindgen::to_value(value)
+}
+
+#[cfg(all(not(feature = "web"), feature = "quickjs"))]
+pub fn create_promise<F, V>(future: F) -> anyhow::Result<OwnedJsPromise>
+where
+    F: core::future::Future<Output = Result<V, anyhow::Error>> + Send + 'static,
+    V: serde::Serialize + Send,
+{
+    use hai_pal::task::get_runtime_handle;
+    use hai_runtime::get_vm;
+
+    let vm = get_vm();
+    let (promise, resolve, reject) =
+        OwnedJsPromise::with_resolvers(vm.context()).expect("Failed to create promise");
+
+    get_runtime_handle().spawn(async move {
+        match future.await {
+            Ok(value) => {
+                resolve
+                    .call(vec![to_js(vm.context().context_raw(), &value).unwrap()])
+                    .expect("Failed to resolve promise");
+            }
+            Err(err) => {
+                reject
+                    .call(vec![
+                        to_js(vm.context().context_raw(), &err.to_string()).unwrap()
+                    ])
+                    .expect("Failed to reject promise");
+            }
+        }
+    });
+
+    Ok(promise)
 }
