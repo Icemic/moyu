56c74f88835e4548ed1cf6ecd6e72502d624cc52
Icemic <bingfeng.web@gmail.com>
2024-07-21 01:20:49 +0800

    chore: move demo to independent folder

--- packages/lib/src/lib.ts
+++ packages/lib/src/lib.ts
@@ -1,5 +1,8 @@
 export * from './react';
 import * as hai from './hai';
 export * from './spring';
+export type * from './declaration';
+export type { HaiEvent } from './hai';
+export type * from './node';
 
 export { hai };
