e512ca109dd9f1f397fb394e0271aee5e94e258a
Icemic <bingfeng.web@gmail.com>
2024-08-03 02:40:45 +0800

    feat: support stretch for nineslice

--- crates/core/src/utils/calculate.rs
+++ crates/core/src/utils/calculate.rs
@@ -10,8 +10,11 @@ pub fn calculate_rect_vertices(
     tex_height: f32,
     origin: &[f32; 2],
     area: &[f32; 4],
+    // for nineslice stretch
+    scale: &[f32; 2],
 ) -> [SpriteVertex; 4] {
     let [x0, y0, x1, y1] = area.to_owned();
+    let [x_scale, y_scale] = scale.to_owned();
 
     // scale size to fit area
     let width = tex_width * (x1 - x0);
@@ -28,26 +31,26 @@ pub fn calculate_rect_vertices(
     let tx = global_transform.z_axis.x;
     let ty = global_transform.z_axis.y;
 
-    let w1 = origin[0] * tex_width;
-    let w0 = w1 + width;
-    let h1 = origin[1] * tex_height;
-    let h0 = h1 + height;
+    let w0 = origin[0] * tex_width;
+    let w1 = w0 + width * x_scale;
+    let h0 = origin[1] * tex_height;
+    let h1 = h0 + height * y_scale;
 
     // left top
-    let p0x = a * w1 + c * h1 + tx;
-    let p0y = b * w1 + d * h1 + ty;
+    let p0x = a * w0 + c * h0 + tx;
+    let p0y = b * w0 + d * h0 + ty;
 
     // left bottom
-    let p1x = a * w0 + c * h1 + tx;
-    let p1y = b * w0 + d * h1 + ty;
+    let p1x = a * w1 + c * h0 + tx;
+    let p1y = b * w1 + d * h0 + ty;
 
     // right top
-    let p2x = a * w0 + c * h0 + tx;
-    let p2y = b * w0 + d * h0 + ty;
+    let p2x = a * w1 + c * h1 + tx;
+    let p2y = b * w1 + d * h1 + ty;
 
     // right bottom
-    let p3x = a * w1 + c * h0 + tx;
-    let p3y = b * w1 + d * h0 + ty;
+    let p3x = a * w0 + c * h1 + tx;
+    let p3y = b * w0 + d * h1 + ty;
 
     let tint = node.base().tint();
     let opacity = node.base().global_opacity();
