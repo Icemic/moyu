f715f41454c0b3439208cdafd2c94aa8241d10bc
Icemic <bingfeng.web@gmail.com>
2025-01-24 21:03:57 +0800

    fix: improve error handling in module loading

--- crates/runtime/src/module.rs
+++ crates/runtime/src/module.rs
@@ -1,40 +1,51 @@
-use log::{error, info};
+use anyhow::Result;
+use log::info;
 
 use hai_pal::config::entry_dir;
 use hai_pal::url::{resolve_package_from, Url};
 use hai_pal::{fs, task};
 
-pub fn module_loader(module_name: &str, _: *mut std::ffi::c_void) -> String {
-    let module_name = Url::parse(module_name).unwrap();
+pub fn module_loader(module_name: &str, _: *mut std::ffi::c_void) -> Result<String> {
+    let module_name = Url::parse(module_name)?;
     let code = match task::block_on(fs::read(&module_name)) {
-        Ok(v) => String::from_utf8(v).unwrap(),
+        Ok(v) => String::from_utf8(v)?,
         Err(err) => {
-            error!("failed to read '{}': {}", module_name, err.to_string());
-            "".to_string()
+            return Err(anyhow::anyhow!(
+                "failed to read '{}': {}",
+                module_name,
+                err.to_string()
+            ));
         }
     };
     info!("module {} loaded", module_name.to_string());
-    code
+    Ok(code)
 }
 
 pub fn module_normalize(
     module_base_name: &str,
     module_name: &str,
     _: *mut std::ffi::c_void,
-) -> String {
+) -> Result<String> {
     let base_dir = if module_base_name == "." {
         entry_dir()
     } else {
-        Url::parse(module_base_name).unwrap()
+        Url::parse(module_base_name)?
     };
 
-    let resolved_module_path = resolve_package_from(module_name, base_dir).unwrap();
-    let resolved_module_path = resolved_module_path.to_string();
+    if let Ok(resolved_module_path) = resolve_package_from(module_name, base_dir) {
+        let resolved_module_path = resolved_module_path.to_string();
 
-    info!(
-        "resolving module {} {} to {}",
-        module_base_name, module_name, resolved_module_path
-    );
+        info!(
+            "resolving module {} {} to {}",
+            module_base_name, module_name, resolved_module_path
+        );
 
-    resolved_module_path
+        Ok(resolved_module_path)
+    } else {
+        Err(anyhow::anyhow!(
+            "failed to resolve module {} {}",
+            module_base_name,
+            module_name
+        ))
+    }
 }
