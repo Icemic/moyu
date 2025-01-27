2731a73dc1dbe44b1f1e8875ad058209060ed60d
Icemic <bingfeng.web@gmail.com>
2025-01-27 10:38:11 +0800

    feat(core): add custom event support to NodeEvent struct

--- crates/core/src/events/node.rs
+++ crates/core/src/events/node.rs
@@ -5,16 +5,22 @@ use crate::traits::Event;
 #[derive(Debug, Clone, Copy, PartialEq, Serialize, Deserialize)]
 pub enum NodeEventKind {
     Destory,
+    Custom,
 }
 
 #[derive(Debug, Clone, PartialEq, Serialize, Deserialize)]
 #[serde(rename_all = "camelCase")]
-pub struct NodeEvent {
+pub struct NodeEvent<T>
+where
+    T: Serialize + Send + 'static,
+{
     pub kind: NodeEventKind,
     pub target_id: u32,
+    pub custom_kind: Option<String>,
+    pub custom_body: Option<T>,
 }
 
-impl Event for NodeEvent {
+impl<T: Serialize + Send + 'static> Event for NodeEvent<T> {
     fn name(&self) -> &'static str {
         "nodeevent"
     }
