225f0d5d605d97d5b4c90899a8d9399b1c34072c
Icemic <bingfeng.web@gmail.com>
2025-01-27 22:04:30 +0800

    feat(kit): add support for custom events in NodeEvent

--- packages/kit/src/events/node.ts
+++ packages/kit/src/events/node.ts
@@ -1,8 +1,11 @@
 export enum NodeEventKind {
   Destory = 'Destory',
+  Custom = 'Custom',
 }
 
 export interface NodeEvent {
   kind: NodeEventKind;
   targetId: number;
+  customKind?: string;
+  customBody?: Record<string, unknown>;
 }
