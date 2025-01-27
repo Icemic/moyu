3c706e51fa4337c89876f2db63b382a31d458321
Icemic <bingfeng.web@gmail.com>
2025-01-27 10:41:46 +0800

    feat(core): implement BindEvent trait for custom event dispatching

--- crates/core/src/traits/event.rs
+++ crates/core/src/traits/event.rs
@@ -1,5 +1,25 @@
 use serde::Serialize;
 
+use crate::events::{NodeEvent, NodeEventKind};
+#[cfg(all(native, feature = "js_runtime"))]
+use crate::utils::dispatch_event::dispatch_event;
+
+#[cfg(all(native, feature = "js_runtime"))]
+use super::Node;
+
 pub trait Event: Serialize + Send + 'static {
     fn name(&self) -> &'static str;
+}
+
+#[cfg(all(native, feature = "js_runtime"))]
+pub trait BindEvent: Node {
+    type Event: Event;
+    fn send_event(&self, key: &str, event: Self::Event) {
+        dispatch_event(NodeEvent {
+            kind: NodeEventKind::Custom,
+            target_id: *self.base().id(),
+            custom_kind: Some(key.to_string()),
+            custom_body: Some(event),
+        });
+    }
 }
