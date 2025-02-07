96a9a6e39d085baa0e8b312143b9893dc72bc87d
Icemic <bingfeng.web@gmail.com>
2025-02-07 22:05:46 +0800

    feat(core): separate core.rs to multiple files

---
+++ crates/core/src/core/input_events.rs
@@ -0,0 +1,416 @@
+use log::error;
+use winit::dpi::PhysicalPosition;
+use winit::event::{ElementState, TouchPhase, WindowEvent};
+use winit::window::{CursorIcon, Window};
+
+use crate::base::*;
+use crate::core::FocusablePayload;
+use crate::events::{MouseEvent, MouseEventKind, TouchEvent, TouchEventKind};
+use crate::state::{DeviceType, PointerLocation, PointerState, MOUSE_IDENTIFIER};
+use crate::utils::dispatch_event::dispatch_event;
+use crate::utils::hit_test::{get_local_logical_position, hit_test};
+
+use super::Core;
+
+macro_rules! get_pointer_state {
+    ($self:ident, $name:ident, $identifier:expr) => {
+        let pointer_map = $self.pointer_map.read();
+        let pointer_state = pointer_map.get(&$identifier);
+
+        if pointer_state.is_none() {
+            error!("Pointer state not found for identifier {}", $identifier);
+            return;
+        }
+
+        let $name = pointer_state.unwrap();
+    };
+
+    ($self:ident, $name:ident, $identifier:expr, $ret:expr) => {
+        let pointer_map = $self.pointer_map.read();
+        let pointer_state = pointer_map.get(&$identifier);
+
+        if pointer_state.is_none() {
+            error!("Pointer state not found for identifier {}", $identifier);
+            return $ret;
+        }
+
+        let $name = pointer_state.unwrap();
+    };
+}
+
+macro_rules! get_pointer_state_mut {
+    ($self:ident, $name:ident, $identifier:expr) => {
+        let mut pointer_map = $self.pointer_map.write();
+        let pointer_state = pointer_map.get_mut(&$identifier);
+
+        if pointer_state.is_none() {
+            error!("Pointer state not found for identifier {}", $identifier);
+            return;
+        }
+
+        let $name = pointer_state.unwrap();
+    };
+
+    ($self:ident, $name:ident, $identifier:expr, $ret:expr) => {
+        let mut pointer_map = $self.pointer_map.write();
+        let pointer_state = pointer_map.get_mut(&$identifier);
+
+        if pointer_state.is_none() {
+            error!("Pointer state not found for identifier {}", $identifier);
+            return $ret;
+        }
+
+        let $name = pointer_state.unwrap();
+    };
+}
+
+impl Core {
+    #[inline(always)]
+    pub fn input(&self, window: &Window, event: &WindowEvent) -> bool {
+        match event {
+            WindowEvent::CursorMoved { position, .. } => {
+                self.handle_pointer_move(window, position, MOUSE_IDENTIFIER);
+                self.handle_pointer_hover(MOUSE_IDENTIFIER, true);
+
+                true
+            }
+            // clear hover node when cursor leaves window
+            WindowEvent::CursorLeft { .. } => {
+                get_pointer_state!(self, pointer_state, MOUSE_IDENTIFIER, true);
+
+                if let Some(last_hover_node) = &pointer_state.current_target {
+                    let target_id = *last_hover_node.node.read().base().id();
+                    dispatch_event(MouseEvent {
+                        kind: MouseEventKind::MouseLeave,
+                        target_id,
+                        bubble_target_ids: last_hover_node.parent_ids.clone(),
+                        location: pointer_state.location,
+                    });
+                }
+                true
+            }
+            WindowEvent::MouseInput { button, state, .. } => {
+                get_pointer_state_mut!(self, pointer_state, MOUSE_IDENTIFIER, true);
+
+                if let Some(last_hover_node) = &pointer_state.current_target {
+                    let target_id = *last_hover_node.node.read().base().id();
+                    let bubble_target_ids = last_hover_node.parent_ids.clone();
+
+                    let location = pointer_state.location;
+
+                    match state {
+                        ElementState::Pressed => {
+                            dispatch_event(MouseEvent {
+                                kind: MouseEventKind::MouseDown,
+                                target_id,
+                                bubble_target_ids,
+                                location,
+                            });
+                            if let winit::event::MouseButton::Left = button {
+                                pointer_state.down_id = Some(target_id);
+                            }
+                        }
+                        ElementState::Released => {
+                            dispatch_event(MouseEvent {
+                                kind: MouseEventKind::MouseUp,
+                                target_id,
+                                bubble_target_ids: bubble_target_ids.clone(),
+                                location,
+                            });
+
+                            let down_id = pointer_state.down_id.take();
+
+                            if down_id == Some(target_id) {
+                                match button {
+                                    winit::event::MouseButton::Left => {
+                                        dispatch_event(MouseEvent {
+                                            kind: MouseEventKind::Click,
+                                            target_id,
+                                            bubble_target_ids,
+                                            location,
+                                        });
+                                    }
+                                    winit::event::MouseButton::Right => {
+                                        dispatch_event(MouseEvent {
+                                            kind: MouseEventKind::ContextMenu,
+                                            target_id,
+                                            bubble_target_ids,
+                                            location,
+                                        });
+                                    }
+                                    winit::event::MouseButton::Back => {
+                                        // do nothing
+                                    }
+                                    winit::event::MouseButton::Forward => {
+                                        // do nothing
+                                    }
+                                    winit::event::MouseButton::Middle => {
+                                        // do nothing
+                                    }
+                                    winit::event::MouseButton::Other(_) => {
+                                        // do nothing
+                                    }
+                                }
+                            }
+                        }
+                    }
+                }
+                true
+            }
+            WindowEvent::Touch(touch) => {
+                let identifier = touch.id as i32;
+
+                self.get_ensure_pointer_state(identifier, DeviceType::Finger(identifier as u32));
+
+                let last_location = {
+                    get_pointer_state!(self, pointer_state, identifier, true);
+                    pointer_state.location
+                };
+
+                self.handle_pointer_move(window, &touch.location, identifier);
+                self.handle_pointer_hover(identifier, touch.phase == TouchPhase::Started);
+
+                get_pointer_state!(self, pointer_state, identifier, true);
+
+                if last_location == pointer_state.location && touch.phase == TouchPhase::Moved {
+                    // ignore duplicated touch move event
+                    return true;
+                }
+
+                if let Some(last_hover_node) = &pointer_state.current_target {
+                    let target_id = *last_hover_node.node.read().base().id();
+                    let bubble_target_ids = last_hover_node.parent_ids.clone();
+
+                    let location = pointer_state.location;
+
+                    match touch.phase {
+                        TouchPhase::Started => {
+                            dispatch_event(TouchEvent {
+                                kind: TouchEventKind::TouchStart,
+                                target_id,
+                                bubble_target_ids,
+                                location,
+                                identifier: Some(touch.id as u32),
+                            });
+                        }
+                        TouchPhase::Moved => {
+                            dispatch_event(TouchEvent {
+                                kind: TouchEventKind::TouchMove,
+                                target_id,
+                                bubble_target_ids,
+                                location,
+                                identifier: Some(touch.id as u32),
+                            });
+                        }
+                        TouchPhase::Ended => {
+                            dispatch_event(TouchEvent {
+                                kind: TouchEventKind::TouchEnd,
+                                target_id,
+                                bubble_target_ids,
+                                location,
+                                identifier: Some(touch.id as u32),
+                            });
+                        }
+                        TouchPhase::Cancelled => {
+                            dispatch_event(TouchEvent {
+                                kind: TouchEventKind::TouchCancel,
+                                target_id,
+                                bubble_target_ids,
+                                location,
+                                identifier: Some(touch.id as u32),
+                            });
+                        }
+                    }
+                }
+                true
+            }
+            _ => false,
+        }
+    }
+
+    /// Handle hover changes on mouse move or touch, and record locations relative to client and screen (always in logical).
+    fn handle_pointer_move(
+        &self,
+        window: &Window,
+        position: &PhysicalPosition<f64>,
+        identifier: i32,
+    ) {
+        get_pointer_state_mut!(self, pointer_state, identifier);
+
+        let stage_size = {
+            let stage_size = self.stage_size.read();
+            *stage_size
+        };
+
+        let (scale, translate_x, translate_y) = {
+            let stage_transform = self.stage_transform.read();
+            *stage_transform
+        };
+
+        let window_position = window.inner_position().unwrap_or_default();
+        let scale_factor = stage_size.scale_factor();
+
+        let global_logical_x = (position.x / scale_factor) as f32;
+        let global_logical_y = (position.y / scale_factor) as f32;
+
+        let screen_logical_x = ((window_position.x as f64 + position.x) / scale_factor) as f32;
+        let screen_logical_y = ((window_position.y as f64 + position.y) / scale_factor) as f32;
+
+        let stage_logical_x = (global_logical_x - translate_x) / scale;
+        let stage_logical_y = (global_logical_y - translate_y) / scale;
+
+        let location = PointerLocation {
+            client_x: stage_logical_x.round() as u32,
+            client_y: stage_logical_y.round() as u32,
+            screen_x: screen_logical_x.round() as u32,
+            screen_y: screen_logical_y.round() as u32,
+            layer_x: 0.,
+            layer_y: 0.,
+        };
+
+        pointer_state.location = location;
+    }
+
+    fn handle_pointer_hover(&self, identifier: i32, refresh_hover_node: bool) {
+        let surface_size = {
+            let surface_size = self.surface_size.read();
+            *surface_size
+        };
+
+        let stage_size = {
+            let stage_size = self.stage_size.read();
+            *stage_size
+        };
+
+        let upload_payload = FocusablePayload {
+            surface_size,
+            stage_size,
+            resource_manager: self.resource_manager.clone(),
+        };
+
+        get_pointer_state_mut!(self, pointer_state, identifier);
+
+        let last_hover_node = &mut pointer_state.current_target;
+
+        if refresh_hover_node {
+            // get node under pointer
+            if let Some(node) = hit_test(
+                &self.root_node,
+                pointer_state.location.client_x as f32,
+                pointer_state.location.client_y as f32,
+                &upload_payload,
+            ) {
+                let node_ref = node.node.read();
+                let (x, y) = get_local_logical_position(
+                    &*node_ref,
+                    pointer_state.location.client_x as f32,
+                    pointer_state.location.client_y as f32,
+                );
+
+                pointer_state.location.layer_x = x;
+                pointer_state.location.layer_y = y;
+
+                drop(node_ref);
+
+                if identifier == MOUSE_IDENTIFIER {
+                    let target_id = *node.node.read().base().id();
+
+                    dispatch_event(MouseEvent {
+                        kind: MouseEventKind::MouseMove,
+                        target_id,
+                        bubble_target_ids: node.parent_ids.clone(),
+                        location: pointer_state.location,
+                    });
+
+                    if let Some(last_hover_node) = last_hover_node {
+                        if last_hover_node == &node {
+                            // do nothing if last focused node is the same as current node
+                            return;
+                        }
+
+                        let node_ref = last_hover_node.node.read();
+                        let (x, y) = get_local_logical_position(
+                            &*node_ref,
+                            pointer_state.location.client_x as f32,
+                            pointer_state.location.client_y as f32,
+                        );
+
+                        let mut location = pointer_state.location;
+
+                        location.layer_x = x;
+                        location.layer_y = y;
+
+                        let target_id = *node_ref.base().id();
+
+                        // drop node guard before dispatching event, since it may cause deadlock
+                        drop(node_ref);
+
+                        // if last focused node is different from current node, it's a mouse leave event and a mouse enter event
+                        dispatch_event(MouseEvent {
+                            kind: MouseEventKind::MouseLeave,
+                            target_id,
+                            bubble_target_ids: last_hover_node.parent_ids.clone(),
+                            location,
+                        });
+                    }
+
+                    // there is always a mouse enter event if current node is different from last focused node (may be None)
+                    dispatch_event(MouseEvent {
+                        kind: MouseEventKind::MouseEnter,
+                        target_id,
+                        bubble_target_ids: node.parent_ids.clone(),
+                        location: pointer_state.location,
+                    });
+                }
+
+                self.set_cursor(node.node.read().base().cursor().clone());
+
+                // record last focused node
+                *last_hover_node = Some(node);
+
+                return;
+            } else {
+                *last_hover_node = None;
+            }
+        }
+
+        // if no node under pointer, it's a mouse leave event if last focused node is not None
+        if let Some(last_hover_node) = last_hover_node {
+            let node_ref = last_hover_node.node.read();
+            let (x, y) = get_local_logical_position(
+                &*node_ref,
+                pointer_state.location.client_x as f32,
+                pointer_state.location.client_y as f32,
+            );
+
+            pointer_state.location.layer_x = x;
+            pointer_state.location.layer_y = y;
+
+            if identifier == MOUSE_IDENTIFIER {
+                let target_id = *node_ref.base().id();
+
+                // drop node guard before dispatching event, since it may cause deadlock
+                drop(node_ref);
+
+                dispatch_event(MouseEvent {
+                    kind: MouseEventKind::MouseLeave,
+                    target_id,
+                    bubble_target_ids: last_hover_node.parent_ids.clone(),
+                    location: pointer_state.location,
+                });
+
+                self.set_cursor(HaiCursor::Visible(CursorIcon::Default));
+            }
+        }
+    }
+
+    /// Check if the pointer state exists, if not, create one.
+    fn get_ensure_pointer_state(&self, identifier: i32, device_type: DeviceType) {
+        let mut pointer_map = self.pointer_map.write();
+        pointer_map.entry(identifier).or_insert_with(|| {
+            let mut pointer_state = PointerState::default();
+            pointer_state.device_type = device_type;
+            pointer_state
+        });
+    }
+}
---
+++ crates/core/src/core/render.rs
@@ -0,0 +1,149 @@
+use std::sync::Arc;
+
+use hai_pal::config::get_engine_config;
+use hai_pal::time::Instant;
+use winit::window::Window;
+
+use crate::utils::walk::walk_nodes_top_bottom;
+
+use super::{Core, RendererUpdatePayload};
+
+impl Core {
+    #[inline(always)]
+    pub fn render(&self, window: &Window) -> Result<(), wgpu::SurfaceError> {
+        // fps
+        #[cfg(native)]
+        if hai_pal::config::get_engine_config().show_fps {
+            let (instant, frames) = &mut *self.frames_in_duration.lock();
+            let duration = instant.elapsed().as_secs_f32();
+            if duration >= 1. {
+                let fps = *frames as f32 / duration;
+
+                self.event_proxy
+                    .send_event(crate::user_event::UserEvent::SetTitle(format!(
+                        "fps: {:.1}",
+                        fps
+                    )))
+                    .unwrap();
+
+                *frames = 1;
+                *instant = Instant::now();
+            } else {
+                *frames += 1;
+            }
+        }
+
+        let surface = self.surface.clone();
+        let device = self.device.clone();
+        let queue = self.queue.clone();
+        let root_node = self.root_node.clone();
+
+        let mut staging_belt = self.staging_belt.lock();
+
+        let surface_size = self.surface_size.read();
+        let stage_size = self.stage_size.read();
+
+        let output = surface.get_current_texture()?;
+        let view = output
+            .texture
+            .create_view(&wgpu::TextureViewDescriptor::default());
+        let mut encoder = {
+            device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Render Command Encoder"),
+            })
+        };
+
+        let mut belt_encoder = {
+            device.create_command_encoder(&wgpu::CommandEncoderDescriptor {
+                label: Some("Belt Command Encoder"),
+            })
+        };
+
+        {
+            let root_node = root_node.read();
+
+            let timestamp = self.instant.elapsed().as_secs_f64();
+            let instant_last = self.instant_last.swap(Arc::new(Instant::now()));
+
+            let upload_payload = RendererUpdatePayload {
+                timestamp,
+                delta: instant_last.elapsed().as_micros() as u32,
+                surface_size: *surface_size,
+                stage_size: *stage_size,
+                resource_manager: self.resource_manager.clone(),
+            };
+
+            let color = &get_engine_config().background_color;
+            let color = wgpu::Color {
+                r: color.r,
+                g: color.g,
+                b: color.b,
+                a: color.a,
+            };
+
+            let mut render_pass = encoder.begin_render_pass(&wgpu::RenderPassDescriptor {
+                label: Some("Render Pass"),
+                color_attachments: &[Some(wgpu::RenderPassColorAttachment {
+                    view: &view,
+                    resolve_target: None,
+                    ops: wgpu::Operations {
+                        load: wgpu::LoadOp::Clear(color),
+                        store: wgpu::StoreOp::Store,
+                    },
+                })],
+                depth_stencil_attachment: None,
+                ..Default::default()
+            });
+
+            render_pass.set_bind_group(0, &self.mvp_bind_group, &[]);
+
+            walk_nodes_top_bottom(&*root_node, &mut |child, parent| {
+                let mut _child = child.write();
+                _child.base_mut().update(parent.base(), &stage_size, false);
+
+                let renderer_type = _child.renderer_type();
+
+                if let Some(current_renderer) = self.renderers.lock().get_mut(renderer_type) {
+                    current_renderer.update(
+                        &mut *_child,
+                        &device,
+                        &queue,
+                        &mut belt_encoder,
+                        &mut staging_belt,
+                        &upload_payload,
+                    );
+
+                    current_renderer.render(&device, &queue, &mut render_pass, &*_child);
+                }
+
+                false
+            });
+        }
+
+        // call after render callback if registered
+        if let Some(after_render_callback) = self.after_render_handler.lock().as_ref() {
+            after_render_callback(
+                &device,
+                &queue,
+                &mut encoder,
+                &output,
+                &view,
+                &mut staging_belt,
+            );
+        }
+
+        staging_belt.finish();
+
+        // TODO: in winit, it is an empty function now, keep an eye on it.
+        window.pre_present_notify();
+
+        queue.submit(
+            std::iter::once(belt_encoder.finish()).chain(std::iter::once(encoder.finish())),
+        );
+        output.present();
+
+        staging_belt.recall();
+
+        Ok(())
+    }
+}
