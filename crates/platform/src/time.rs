d9832ed4f9aff57c6b15fcd711818c9e0a0f9204
Icemic <bingfeng.web@gmail.com>
2025-06-18 04:14:47 +0800

    feat(platform): replace web-time with wasmtimer which has `sleep` support

--- crates/platform/src/time.rs
+++ crates/platform/src/time.rs
@@ -1,5 +1,13 @@
 #[cfg(native)]
 pub use std::time::*;
+#[cfg(native)]
+pub use tokio::time::{
+    interval, interval_at, sleep, sleep_until, timeout, timeout_at, Interval, Sleep, Timeout,
+};
 
 #[cfg(web)]
-pub use web_time::*;
+pub use wasmtimer::std::*;
+#[cfg(web)]
+pub use wasmtimer::tokio::{
+    interval, interval_at, sleep, sleep_until, timeout, timeout_at, Interval, Sleep, Timeout,
+};
