04047f073281e74bf8e4389d93c3287aa3d16b98
Icemic <bingfeng.web@gmail.com>
2025-08-06 04:58:35 +0800

    feat(core): add Snapshot struct and SnapshotFormat enum with serialization support

--- crates/core/src/base/mod.rs
+++ crates/core/src/base/mod.rs
@@ -1,6 +1,7 @@
 mod cursor;
 mod mvp_matrix;
 mod point;
+mod snapshot;
 mod surface_size;
 mod transform;
 mod vertex;
@@ -8,6 +9,7 @@ mod vertex;
 pub use cursor::*;
 pub use mvp_matrix::*;
 pub use point::*;
+pub use snapshot::*;
 pub use surface_size::*;
 pub use transform::*;
 pub use vertex::*;
