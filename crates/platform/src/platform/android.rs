c3dd37699f8c456076226ad5aed0f8be20f6924d
Icemic <bingfeng.web@gmail.com>
2025-09-28 15:19:23 +0800

    refactor: update InvisibleHand usage

--- crates/platform/src/platform/android.rs
+++ crates/platform/src/platform/android.rs
@@ -1,15 +1,13 @@
 use crate::visible_hand::{InvisibleHand, VisibleHand};
 use android_activity::AndroidApp;
 
-static mut ANDROID_APP: InvisibleHand<AndroidApp> = InvisibleHand::new();
+static ANDROID_APP: InvisibleHand<AndroidApp> = InvisibleHand::new();
 
 pub fn setup_android(app: &AndroidApp) -> VisibleHand<AndroidApp> {
-    unsafe {
-        ANDROID_APP.set(app.clone()).expect("Failed to set handle.");
-        ANDROID_APP.intervent()
-    }
+    ANDROID_APP.set(app.clone()).expect("Failed to set handle.");
+    ANDROID_APP.intervent()
 }
 
 pub fn get_android_app<'a>() -> &'a AndroidApp {
-    unsafe { ANDROID_APP.get() }
+    ANDROID_APP.get()
 }
