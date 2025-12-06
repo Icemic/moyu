d37d03cbdddc9d3479821d29acc553907e6e0c68
Icemic <bingfeng.web@gmail.com>
2025-12-06 23:02:38 +0800

    feat: support passing configuration from JavaScript when initializing Moyu on web platform.

--- crates/moyu/src/lib.rs
+++ crates/moyu/src/lib.rs
@@ -20,9 +20,16 @@ async fn android_main(app: AndroidApp) {
 
 #[cfg(web)]
 #[cfg_attr(web, wasm_bindgen::prelude::wasm_bindgen)]
-pub async fn moyu_init(element_id: &str) {
+pub async fn moyu_init(element_id: &str, config: Option<wasm_bindgen::JsValue>) {
     moyu_pal::logger::setup();
-    moyu_pal::config::setup().await;
+
+    if let Some(config) = config
+        && config.is_object()
+    {
+        moyu_pal::config::setup_with_wasm_config(config);
+    } else {
+        moyu_pal::config::setup().await;
+    }
 
     let event_loop = moyu_core::surface::create_eventloop();
     entry::main_entry(event_loop, element_id).await;
