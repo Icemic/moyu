baca4c5b22b91fb39f52e96c5e6b582087db00a3
Icemic <bingfeng.web@gmail.com>
2025-12-29 23:25:34 +0800

    chore(nodes): remove unused video and yuv_sprite modules

--- crates/nodes/src/nodes/mod.rs
+++ crates/nodes/src/nodes/mod.rs
@@ -1,15 +1,7 @@
 mod sprite;
 #[cfg(feature = "text")]
 mod text;
-#[cfg(feature = "video")]
-mod video;
-#[cfg(feature = "yuv_sprite")]
-mod yuv_sprite;
 
 pub use sprite::*;
 #[cfg(feature = "text")]
 pub use text::*;
-#[cfg(feature = "video")]
-pub use video::*;
-#[cfg(feature = "yuv_sprite")]
-pub use yuv_sprite::*;
