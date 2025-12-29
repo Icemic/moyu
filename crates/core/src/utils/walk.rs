f4777d7d601955c4437229fb2fc8f66c778b385a
Icemic <bingfeng.web@gmail.com>
2025-12-30 00:44:26 +0800

    feat(core): add walk_nodes_enter_leave function for top-to-bottom traversal with callbacks

--- crates/core/src/utils/walk.rs
+++ crates/core/src/utils/walk.rs
@@ -28,6 +28,35 @@ where
     false
 }
 
+/// walk through all node-like ones from top to bottom with enter and leave callbacks
+pub fn walk_nodes_enter_leave<E, L>(root_node: &dyn Node, enter: &mut E, leave: &mut L) -> bool
+where
+    E: FnMut(NodeLock, &dyn Node) -> bool,
+    L: FnMut(NodeLock, &dyn Node),
+{
+    let children = root_node.base().children();
+    for child in children.iter() {
+        let should_end = enter(child.clone(), root_node);
+
+        if should_end {
+            return true;
+        }
+
+        {
+            let child_ref = child.read();
+            if !child_ref.base().children().is_empty() && child_ref.base().visible() {
+                let should_end = walk_nodes_enter_leave(&*child_ref, enter, leave);
+                if should_end {
+                    return true;
+                }
+            }
+        }
+
+        leave(child.clone(), root_node);
+    }
+    false
+}
+
 /// walk through all node-like ones from bottom to top,
 /// due that the depth should not big, recursive is acceptable
 pub fn walk_nodes_bottom_top<T>(
