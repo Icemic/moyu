7b20c16c8f65aa2fd12f8f3e038a6cc35535f0f0
Icemic <bingfeng.web@gmail.com>
2026-01-13 10:21:02 +0800

    feat(kit): update bindings

--- packages/kit/src/declaration.ts
+++ packages/kit/src/declaration.ts
@@ -5,6 +5,12 @@ import type { BubbleEvent } from './events/base';
 import type { MouseEvent } from './events/mouse';
 import type { TouchEvent } from './events/touch';
 import type { KeyboardEvent } from './events/keyboard';
+import { NodeProps } from './bindings/NodeProps';
+import { ClipProps } from './bindings/ClipProps';
+import { FilterProps } from './bindings/FilterProps';
+import { BackdropProps } from './bindings/BackdropProps';
+import { SpriteProps } from './bindings/SpriteProps';
+import { TextProps } from './bindings/TextProps';
 
 export type Tuple2 = [number, number] | readonly [number, number];
 export type Tuple3 = [number, number, number] | readonly [number, number, number];
@@ -35,140 +41,22 @@ export interface MoyuListenerAttributes {
   onTouchCancel?: MoyuEventHandler<TouchEvent>;
 }
 
-export interface MoyuNodeAttributes extends MoyuListenerAttributes {
-  label?: string;
-  x?: number;
-  y?: number;
-  // the anchor for node, values range from 0.0 to 1.0.
-  anchor?: Tuple2;
-  // the pivot on ratation, values in pixels.
-  pivot?: Tuple2;
-  scale?: number;
-  scaleX?: number;
-  scaleY?: number;
-  rotation?: number;
-  skew?: number;
-  skewX?: number;
-  skewY?: number;
-  visible?: boolean;
-  tint?: string;
-  opacity?: number;
-  children?: ReactNode;
-  interactive?: boolean;
-  cursor?: Cursor;
-}
-
-export type FilterKind =
-  | { type: 'blur'; radius: number }
-  | {
-      type: 'colorAdjust';
-      brightness?: number;
-      contrast?: number;
-      saturation?: number;
-      hue?: number;
-    };
+export type MoyuNodeAttributes = MoyuListenerAttributes &
+  NodeProps & {
+    children?: ReactNode;
+  };
 
 export type MoyuContainerAttributes = MoyuNodeAttributes;
-export interface MoyuClipAttribute extends MoyuNodeAttributes {
-  width?: number;
-  height?: number;
-}
-export interface MoyuFilterAttribute extends MoyuNodeAttributes {
-  filters?: FilterKind[];
-}
-export interface MoyuBackdropAttribute extends MoyuNodeAttributes {
-  filters?: FilterKind[];
-  width: number;
-  height: number;
-}
-
-export interface MoyuSpriteAttribute extends MoyuNodeAttributes {
-  src?: string;
-  area?: Tuple4;
-  mode?: 'normal' | 'nineslice';
-  bounds?: Tuple4;
-  nineSliceMode?: 'stretch' | 'repeat' | 'mirror' | 'blank';
-  targetWidth?: number;
-  targetHeight?: number;
-}
-
-export type Color = number | string;
-
-export type Cursor =
-  | 'hidden'
-  | 'default'
-  | 'context-menu'
-  | 'help'
-  | 'pointer'
-  | 'progress'
-  | 'wait'
-  | 'cell'
-  | 'crosshair'
-  | 'text'
-  | 'vertical-text'
-  | 'alias'
-  | 'copy'
-  | 'move'
-  | 'no-drop'
-  | 'not-allowed'
-  | 'grab'
-  | 'grabbing'
-  | 'e-resize'
-  | 'n-resize'
-  | 'ne-resize'
-  | 'nw-resize'
-  | 's-resize'
-  | 'se-resize'
-  | 'sw-resize'
-  | 'w-resize'
-  | 'ew-resize'
-  | 'ns-resize'
-  | 'nesw-resize'
-  | 'nwse-resize'
-  | 'col-resize'
-  | 'row-resize'
-  | 'all-scroll'
-  | 'zoom-in'
-  | 'zoom-out';
-
-export interface MoyuTextAttribute extends MoyuNodeAttributes {
-  text?: string;
-  printMode?: 'instant' | 'typewriter' | 'printer';
-  printSpeed?: number;
-
-  /* layout styles */
-  /// the writing direction of the text in the box,
-  /// only `Horizontal` (right-to-left) or `Vertical` (top-to-bottom) is valid.
-  direction?: 'horizontal' | 'vertical';
-  /// the width of box.
-  boxWidth?: number;
-  /// the height of box.
-  boxHeight?: number;
-  /// the size of the glyph grid which each character be fit to, usually equals to `font_size`.
-  glyphGridSize?: number;
-
-  /* text styles */
-  fontSize?: number;
-  fillColor?: Color;
-  lineHeight?: number;
-  indent?: number;
-
-  stroke?: boolean;
-  shadow?: boolean;
-
-  strokeColor?: Color;
-  strokeWidth?: number;
-
-  shadowColor?: Color;
-  shadowOffsetX?: number;
-  shadowOffsetY?: number;
-  shadowBlur?: number;
-  shadowWidth?: number;
-
-  onStart?: () => void;
-  onFinish?: () => void;
-  onProgress?: (progress: number) => void;
-}
+export type MoyuClipAttributes = ClipProps & MoyuNodeAttributes;
+export type MoyuFilterAttributes = FilterProps & MoyuNodeAttributes;
+export type MoyuBackdropAttributes = BackdropProps & MoyuNodeAttributes;
+export type MoyuSpriteAttributes = SpriteProps & MoyuNodeAttributes;
+export type MoyuTextAttributes = TextProps &
+  MoyuNodeAttributes & {
+    onStart?: () => void;
+    onFinish?: () => void;
+    onProgress?: (progress: number) => void;
+  };
 
 // eslint-disable-next-line @typescript-eslint/no-namespace
 export declare namespace JSX {
@@ -189,10 +77,10 @@ export declare namespace JSX {
 
   interface IntrinsicElements {
     container: DetailedMoyuProps<MoyuContainerAttributes>;
-    sprite: DetailedMoyuProps<MoyuSpriteAttribute>;
-    clip: DetailedMoyuProps<MoyuClipAttribute>;
-    filter: DetailedMoyuProps<MoyuFilterAttribute>;
-    backdrop: DetailedMoyuProps<MoyuBackdropAttribute>;
-    text: DetailedMoyuProps<MoyuTextAttribute>;
+    sprite: DetailedMoyuProps<MoyuSpriteAttributes>;
+    clip: DetailedMoyuProps<MoyuClipAttributes>;
+    filter: DetailedMoyuProps<MoyuFilterAttributes>;
+    backdrop: DetailedMoyuProps<MoyuBackdropAttributes>;
+    text: DetailedMoyuProps<MoyuTextAttributes>;
   }
 }
