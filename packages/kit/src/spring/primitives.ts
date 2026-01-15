df77877849e87b193d10389a0cf4b86340affafb
Icemic <bingfeng.web@gmail.com>
2026-01-15 20:55:16 +0800

    feat(kit): add animation primitive type, add AnimationFormat and AnimationProps types

--- packages/kit/src/spring/primitives.ts
+++ packages/kit/src/spring/primitives.ts
@@ -1,11 +1,4 @@
 import type { JSX } from '../declaration';
 export type Primitives = keyof JSX.IntrinsicElements;
 
-export const primitives: Primitives[] = [
-  'container',
-  'sprite',
-  'clip',
-  'filter',
-  'backdrop',
-  'text',
-];
+export const primitives: Primitives[] = ['container', 'sprite', 'text', 'clip', 'filter', 'backdrop', 'animation'];
