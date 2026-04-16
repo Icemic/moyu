1226492c4260795e8a19f4521d30324b2fad4c97
Icemic <bingfeng.web@gmail.com>
2026-04-17 02:01:35 +0800

    fix(kit): update frameLoop to 'always' and remove unused raf code

--- packages/kit/src/spring/index.ts
+++ packages/kit/src/spring/index.ts
@@ -1,5 +1,5 @@
 import { createHost } from '@react-spring/animated';
-import { Globals, colors, createStringInterpolator, raf } from '@react-spring/shared';
+import { Globals, colors, createStringInterpolator } from '@react-spring/shared';
 import type { Node as MoyuNode } from '../node';
 import type { WithAnimated } from './animated';
 import { primitives } from './primitives';
@@ -7,13 +7,9 @@ import { primitives } from './primitives';
 Globals.assign({
   createStringInterpolator,
   colors,
-  frameLoop: 'demand',
+  frameLoop: 'always',
+  requestAnimationFrame: (cb) => requestAnimationFrame(cb),
 });
-
-// Let r3f drive the frameloop.
-setInterval(() => {
-  raf.advance();
-}, 10);
 
 const host = createHost(primitives, {
   applyAnimatedValues(instance: MoyuNode, props) {
